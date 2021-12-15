a3dcadaf60938f2d1af0ba1e44f622efd5b2f5bf
Mario Sever <34382956+mariosever@users.noreply.github.com>
2021-12-15 12:54:59 +0100

    Update main.js

--- Funkcije 03/main.js
+++ Funkcije 03/main.js
@@ -1,11 +1,2 @@
 // Kalkulator psećih godina
 
-function dogAge(age) {
-    let dogYears = 7*age;
-    alert("Your doggie is " + dogYears + " years old in dog years!");
-}
-
-let age = prompt("How old is your doggie in human years? ");
-
-
-dogAge(age);

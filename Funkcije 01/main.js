578ba7f739c98282f81f6b3f3571437253afbbc9
Mario Sever <34382956+mariosever@users.noreply.github.com>
2021-12-15 12:53:31 +0100

    Add files via upload

---
+++ Funkcije 01/main.js
@@ -0,0 +1,40 @@
+// JavaScript funkcije
+
+// definiramo funkciju
+function mojaFunkcija() {
+
+    alert("Ovo je poruka iz naše funkcije!");
+
+}
+
+// pozivamo funkciju
+// mojaFunkcija();
+
+
+function zbroji(a,b) {
+
+    c = a + b;
+    alert("Zbroj a i b je jednako: " + c);
+
+}
+
+// zbroji(4,3);
+// zbroji(53, 69);
+
+
+let x = 0;
+
+function doseg() {
+
+    let x = 100;
+
+    alert(x);
+
+}
+
+alert(x);
+
+doseg();
+
+
+
---
+++ Funkcije 03/main.js
@@ -0,0 +1,11 @@
+// Kalkulator psećih godina
+
+function dogAge(age) {
+    let dogYears = 7*age;
+    alert("Your doggie is " + dogYears + " years old in dog years!");
+}
+
+let age = prompt("How old is your doggie in human years? ");
+
+
+dogAge(age);
